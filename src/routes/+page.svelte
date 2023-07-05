da9a45aac156f66513660903d3763b16092c909b
Amit Shah <mylifenp@yahoo.com>
2023-07-05 22:17:40 +0200

    test reactivity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <h1 class="text-3xl font-bold line-through">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<a href="/about">About</a>
+
 <style lang="postcss">
 	:global(html) {
-		background-color: theme(colors.gray.100);
+		background-color: theme(colors.gray.200);
 	}
 </style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	let x = 0;
+	let y = 0;
+
+	function yPlusAValue(value: number) {
+		return y + value;
+	}
+	$: total = yPlusAValue(x);
+</script>
+
+<div class="header">
+	<div class="text-9xl text-cyan-400 m-12">Total: {total}</div>
+
+	<div>
+		<button
+			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+			on:click={() => x++}>Increment X</button
+		>
+		<button
+			class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+			on:click={() => x--}>Decrease X</button
+		>
+	</div>
+</div>
+
+<style lang="postcss">
+	.header {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
+		margin: 2rem;
+	}
+</style>
